3154b00d7b23ac13072a549396157251c1af06a9
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-03 13:05:29 -0600

    Clean up controllers and make them JSONApi compliant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,61 +5,6 @@ class ApplicationController < ActionController::Base
 	before_filter :authenticate_school_from_token!
 	before_filter :authenticate_school!
 
-	def flash_message type, text
-		flash[type] ||= []
-		flash[type] << text
-	end
-
-	def book_lookup isbn
-		if (a = current_school.aliases.find_by(:name => isbn))
-			a.book
-		elsif (b = current_school.books.find_by(:isbn => isbn))
-			b
-		else
-			nil
-		end
-	end
-
-	def log_in school
-		session[:school_id] = school.id
-	end
-
-	def log_out
-		session.delete :school_id
-	end
-
-	def current_school
-		@current_school ||= School.find_by(:id => session[:school_id])
-	end
-
-	def current_school? school
-		school == current_school
-	end
-
-	def logged_in?
-		!current_school.nil?
-	end
-
-	helper_method :logged_in?
-	helper_method :current_school
-
-	def logged_in_school
-		unless logged_in?
-			store_location
-			flash_message :danger, "Bitte einloggen"
-			redirect_to login_url
-		end
-	end
-
-	def redirect_back_or default
-		redirect_to(session[:forwarding_url] || default)
-		session.delete :forwarding_url
-	end
-
-	def store_location
-		session[:forwarding_url] = request.url if request.get?
-	end
-
 	private
 		def authenticate_school_from_token!
 			authenticate_with_http_token do |token, options|
