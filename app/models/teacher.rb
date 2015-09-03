7d0da8d360ff15b9d2c89ca472e8689b5d8f608a
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-03 13:21:47 -0600

    Remove `Teacher#lend*` methods

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,20 +5,4 @@ class Teacher < ActiveRecord::Base
 
 	validates :name, :presence => true
 	validates :school, :presence => true
-
-	def lend_book(book)
-		self.lendings.create(:book => book) if book
-	end
-
-	def return_book(book)
-		if book && (l = self.lendings.find_by(:book => book))
-			l.destroy
-		end
-	end
-
-	def lend_base_set(book)
-	end
-
-	def return_base_set(book)
-	end
 end
