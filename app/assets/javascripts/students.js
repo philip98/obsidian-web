a50aa0237a8c783b6d499c948fa5fe634094908e
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-03 13:20:55 -0600

    Same for the assets

--- app/assets/javascripts/students.js
+++
@@ -1,8 +0,0 @@
-function addAjaxToListSelect() {
-	var field = $("#listSelect");
-	field.change(function() {
-		$.ajax(field.attr("data-url"), {data: {list: field.val()}, dataType: "script"});
-	});
-}
-
-$(document).ready(addAjaxToListSelect);
