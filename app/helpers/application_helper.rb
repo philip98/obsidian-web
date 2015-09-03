8786e145fc755dbd922be7955a8a2263f029b074
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-03 13:12:42 -0600

    Clean up helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,2 @@
 module ApplicationHelper
-	def full_title(page_title = "")
-		base_title = "Obsidian Web"
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}"
-		end
-	end
-
-	def render_flash
-		rendered = []
-		flash.each do |type, messages|
-			messages.each do |m|
-				rendered << render(:partial => "shared/flash", :locals => {:type => type, :message => m}) unless m.blank?
-			end
-		end
-		rendered.join("").html_safe
-	end
 end
