ad30cd9fc37b0f6bd58de6587da90cad5ebe6148
Philip Schlösser <philipschloesser98@gmail.com>
2015-09-19 12:58:34 +0200

    Fix wrong order of before_filters
    `authenticate_from_token!` used to be called after `context`, which jeopardises
    the whole auth system.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,26 @@ class ApplicationController < JSONAPI::ResourceController
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :null_session
-	before_action :authenticate_from_token!
+	prepend_before_filter :authenticate_from_token!
 #	before_filter :authenticate_school!
 
 	def context
-		{:current_school => current_school}
+		{:current_school => current_school || @@current_school}
 	end
 
 	private
 		def authenticate_from_token!
 			logger.info "ApplicationController#authenticate_school_from_token!"
-			res = authenticate_with_http_token do |token, options|
+			authenticate_with_http_token do |token, options|
 				secret_id = options[:secret_id]
-				logger.debug "secret_id: #{secret_id}"
-				logger.debug "secret: #{token}"
 				current_token = AuthenticationToken.find_authenticated :secret_id => secret_id, :secret => token
 				if current_token
 					sign_in current_token.school
+					@@current_school = current_token.school
 				else
 					nil
 				end
 			end
-			render :nothing => true, :status => :forbidden unless res
+			render :nothing => true, :status => :forbidden unless signed_in?
 		end
 end
